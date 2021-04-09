bd6c6356955bd1bd19b2a3a102717bb8f59e32dd
424253536 <424253536@qq.com>
2021-04-09 16:06:16 +0800

    sdf水电费水电费水电费

--- src/main/java/WebMallApplication.java
+++ src/main/java/WebMallApplication.java
@@ -13,6 +13,7 @@ public class WebMallApplication {
 
     public static void main(String[] args) {
         System.out.println("11111111111111111111");
+        System.out.println("test");
         SpringApplication.run(WebMallApplication.class,args);
     }
 }
